6f9d42728cdb77dc58b0cb5f8464c8c8f304b942
Kunal Thakkar <kunalmthakkar@gmail.com>
2021-03-24 12:40:46 +0530

    Shuffle pin to easy wiring

--- inc/main.h
+++ inc/main.h
@@ -5,17 +5,21 @@
 #define I2C_SCL_PIN			GPIO_PIN_4
 #define I2C_SDA_PIN			GPIO_PIN_5
 
+#define OSC_PORT			GPIOA
+#define OSC_IN_PIN		GPIO_PIN_1
+#define OSC_OUT_PIN		GPIO_PIN_2
+
 #define BUTTON_SET_PORT	    GPIOD
-#define BUTTON_SET_PIN	    GPIO_PIN_3
+#define BUTTON_SET_PIN	    GPIO_PIN_5
 
 #define BUTTON_UP_PORT	    GPIOD
-#define BUTTON_UP_PIN		GPIO_PIN_5
+#define BUTTON_UP_PIN		GPIO_PIN_6
 
 #define BUTTON_DW_PORT	    GPIOD
-#define BUTTON_DW_PIN		GPIO_PIN_6
+#define BUTTON_DW_PIN		GPIO_PIN_4
 
-#define PN2222_PORT			GPIOD
-#define PN2222_PIN			GPIO_PIN_4
+#define PN2222_PORT			GPIOA
+#define PN2222_PIN			GPIO_PIN_3
 
 #define LCD_RS_PORT 		GPIOD
 #define LCD_RS_PIN			GPIO_PIN_2
